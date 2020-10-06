b3e6838780f3041bc6f42f6fa30a728192ae37d3
KokiWatari <watari-kouki-wh@ynu.jp>
2020-10-06 17:46:09 +0900

    10/6

--- LLGMN/Layer.h
+++ LLGMN/Layer.h
@@ -11,66 +11,68 @@
 using namespace std;
 class Layer {
 private:
-    //D:入力次元、H:非線形変換後の次元、M:コンポーネント数、K:クラスの数
-    int D, H, M, K;
+    //D:入力次元、M:コンポーネント数、K:クラス数
+    const int D, M, K;
+    //H:非線形変換後の次元
+    int H;
     // input layer
     vector<double> input1;
     vector<vector<double>> input2;
     vector<double> input3;
     // output layer
-    vector<double> X;
+    vector<double> x;
     vector<double> output1;
     vector<vector<double>> output2;
     vector<double> y;
-    // error value
-    vector<double> error;
     // weight
     vector<vector<vector<double>>> weight;
-    //第2層途中式用
-    double sum;
-    //評価関数
-    vector<double> J;
     //微分
     vector<vector<vector<double>>> dj_dw;
-    vector<vector<double>> dj_dw_sum;
     //学習率
-    double eta;
+    const double eta;
 
 
 public:
-    vector<double> forward(vector<double> input_data) {//input_data:入力ベクトル?
+    Layer(int D,int K,int M,double eta);
+    virtual ~Layer() {};
+
+    vector<double> forward(vector<double> input_data) {
         //非線形変換
 
 
 
 
-        //calculation of first layer
-        for (int h = 0; h <= H; h++) {
-            input1[h] = X[h];
+        //第１層
+        for (int h = 0; h < H; h++) {
+            input1[h] = x[h];
             output1[h] = input1[h];
         }
-        //caluculation of second layer
-        for (int k = 0; k <= K; k++) {
-            for (int m = 0; m <= M; m++) {
+        //第２層
+        double sum = 0;
+        for (int k = 0; k < K; k++) {
+            for (int m = 0; m < M; m++) {
                 if ((k == K) && (m == M)) {
+                    for (int h = 0; h < H; h++) {
+                        weight[h][K][M] = 0;
+                    }
                     input2[k][m] = 0;
                 }
                 else {
-                    for (int h = 0; h <= H; h++) {
+                    for (int h = 0; h < H; h++) {
                         input2[k][m] += output1[h] * weight[h][k][m];
                     }
                 }
                 sum += exp(input2[k][m]);
             }
         }
-        for (int k = 0; k <= K; k++) {
-            for (int m = 0; m <= M; m++) {
+        for (int k = 0; k < K; k++) {
+            for (int m = 0; m < M; m++) {
                 output2[k][m] = exp(input2[k][m]) / sum;
             }
         }
-        //caluculation of third layer
-        for (int k = 0; k <= K; k++) {
-            for (int m = 0; m <= M; m++) {
+        //第３層
+        for (int k = 0; k < K; k++) {
+            for (int m = 0; m < M; m++) {
                 input3[k] += output2[k][m];
             }
             y[k] = input3[k];
@@ -78,45 +80,88 @@ public:
         return y;
     }
 
+
     void learn_online(vector<vector<double>>& input_data, vector<vector<double>>& input_label) {
-        vector<double> Y = forward(input_data);
-        for (int times = 0; times < 10001; ++times) {
-   
-            for (int k = 0; k <= K; k++) {
-                for (int m = 0; m <= M; m++) {
-                    for (int h = 0; h <= H; h++) {
-                        error[k] = Y[k] - input_label[k];
-                        dj_dw[h][k][m] = error[k] * (output2[k][m] / Y[k]) * X[h];
-                        weight[h][k][m] -= eta * dj_dw[h][k][m];
+        srand((unsigned int)time(NULL));
+        double evaluation = -100;
+        for (int times = 0; times < 10001 && evaluation / input_data.size() < -0.01; ++times) {
+            evaluation = 0;
+            for (int d = 0; d < input_data.size(); d++) {
+                int a = rand() % input_data.size();
+                vector<double> Y = forward(input_data[a]);
+                for (int k = 0; k < K; k++) {
+                    for (int m = 0; m < M; m++) {
+                        for (int h = 0; h < H; h++) {
+                            dj_dw[h][k][m] = (Y[k] - input_label[a][k]) * (output2[k][m] / Y[k]) * x[h];
+                            //重みの更新(逐次)
+                            weight[h][k][m] -= eta * dj_dw[h][k][m];
+                        }
                     }
                 }
+                //評価関数(交差エントロピー誤差)の計算
+                double j = 0;
+                for (int k = 0; k < K; k++) {
+                    j += input_label[a][k] * log(Y[k]);
+                }
+                evaluation -= j;
             }
-
+            if (times % 10 == 0) {
+                cout << "times:" << times << "evaluation, :" << evaluation / input_data.size() << endl;
+            }
         }
-
-
     }
 
-    void learn_patch(vector<vector<double>>& input_data, vector<vector<double>>& input_label) {
-        vector<double> Y = forward(input_data);
-        for (int times = 0; times < 10001; ++times) {
 
-            for (int k = 0; k <= K; k++) {
-                for (int m = 0; m <= M; m++) {
-                    for (int h = 0; h <= H; h++) {
-                        error[k] = Y[k] - input_label[k];
-                        dj_dw[h][k][m] = error[k] * (output2[k][m] / Y[k]) * X[h];
-                        dj_dw_sum[k][m] += dj_dw[h][k][m];
+    void learn_patch(vector<vector<double>>& input_data, vector<vector<double>>& input_label) {
+        double evaluation = -100;
+        double dj_dw_sum1 = 100;
+        double dj_dw_sum2 = 100;
+        for (int times = 0; times < 10001 && evaluation / input_data.size() < -0.01; ++times) {
+            evaluation = 0;
+            dj_dw_sum2 = 0;
+            for (int d = 0; d < input_data.size(); d++) {
+                vector<double> Y = forward(input_data[d]);
+                dj_dw_sum1 = 0;
+                for (int k = 0; k < K; k++) {
+                    for (int m = 0; m < M; m++) {
+                        for (int h = 0; h < H; h++) {
+                            dj_dw[h][k][m] = (Y[k] - input_label[d][k]) * (output2[k][m] / Y[k]) * x[h];
+                            dj_dw_sum1 += dj_dw[h][k][m];
+                        }
                     }
-                    for (int h = 0; h <= H; h++) {
-                        weight[h][k][m] -= eta * dj_dw_sum[k][m] / H;
+                }
+                dj_dw_sum2 += dj_dw_sum1 / (H * K * M);
+                //評価関数(交差エントロピー誤差)の計算
+                double j = 0;
+                for (int k = 0; k < K; k++) {
+                    j += input_label[d][k] * log(Y[k]);
+                }
+                evaluation -= j;
+            }
+            //重みの更新(一括)
+            for (int k = 0; k < K; k++) {
+                for (int m = 0; m < M; m++) {
+                    for (int h = 0; h < H; h++) {
+                        weight[h][k][m] -= eta * dj_dw_sum2 / input_data.size();
                     }
                 }
             }
+            if (times % 10 == 0) {
+                cout << "times:" << times << "evaluation, :" << evaluation / input_data.size() << endl;
+            }
+        }    
+    }
 
+
+    void init_weight() {
+        srand((unsigned int)time(NULL));
+        for (int k = 0; k < K; k++) {
+            for (int m = 0; m < M; m++) {
+                for (int h = 0; h < H; h++) {
+                    weight[h][k][m] = rand() % 2;
+                }
+            }
         }
-
-
     }
 
 
--- LLGMN/Source.cpp
+++ LLGMN/Source.cpp
@@ -7,22 +7,69 @@
 using namespace std;
 vector<vector<double>> get_vector_from_file(string filename);
 
+Layer::Layer(int D, int K, int M, double eta)
+	:D(D)
+	,K(K)
+	,M(M)
+	,eta(eta)
+{
+	x = vector<double>(H);
+	input1 = vector<double>(H);
+	input2 = vector<vector<double>>(K, vector<double>(M, 0));
+	input3 = vector<double>(K);
+	output1 = vector<double>(H);
+	output2 = vector<vector<double>>(K, vector<double>(M, 0));
+	y = vector<double>(K);
+	weight = vector<vector<vector<double>>>(H, vector<vector<double>>(K, vector<double>(M, 0)));
+	dj_dw = vector<vector<vector<double>>>(H, vector<vector<double>>(K, vector<double>(M, 0)));
+	init_weight();
+}
+
+
 int main() {
-
+	int d, k, m,is_online;
+	cout << "入力次元(今回は800):";
+	cin >> d;
+	cout << "クラス数(今回は4):";
+	cin >> k;
+	cout << "コンポーネント数:";
+	cin >> m;
+	cout << "一括学習:0, 逐次学習:1" << endl << ":";
+	cin >> is_online;
 	// データ読み取り部
-	/*
-	vector<vector<double>> input_data = get_vector_from_file("lea_sig");
-	vector<vector<double>> output_data = get_vector_from_file("lea_T_sig");
+	vector<vector<double>> input_data = get_vector_from_file("lea_sig.csv");
+	vector<vector<double>> input_label = get_vector_from_file("lea_T_sig.csv");
 	vector<vector<double>> test_input_data = get_vector_from_file("dis_sig.csv");
-	vector<vector<double>> test_output_data = get_vector_from_file("dis_T_sig.csv");
-	*/
-	
+	vector<vector<double>> test_input_label = get_vector_from_file("dis_T_sig.csv");
+	//Layerクラス　第4引数が学習率
+	Layer layer(d, k, m, 0.1);
 
+	if (is_online) {
+		layer.learn_online(input_data, input_label);
+	}
+	else {
+		layer.learn_patch(input_data, input_label);
+	}
 
+	vector<vector<double>> output(input_label.size(), vector<double>(input_label[0].size()));
+	printf("学習データの出力結果\n");
+	for (int i = 0; i < input_data.size(); i++) {
+		output[i] = layer.forward(input_data[i]);
+		for (int j = 0; j < input_data[0].size(); j++) {
+			printf("%lf ,", output[i][j]);
+		}
+		printf("\n");
+	}
 
-
-
-
+	vector<vector<double>> test_output(test_input_label.size(), vector<double>(test_input_label[0].size()));
+	printf("テストデータの出力結果\n");
+	for (int i = 0; i < test_input_data.size(); i++) {
+		test_output[i] = layer.forward(test_input_data[i]);
+		for (int j = 0; j < test_input_data[0].size(); j++) {
+			printf("%lf ,", test_output[i][j]);
+		}
+		printf("\n");
+	}
 
 
 	return 0;
